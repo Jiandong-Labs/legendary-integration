0a40fcdbac1294d7105f780b9192fbcb6c97c5c0
Jiandong Ma <jiandong.ma.cn@gmail.com>
2026-04-23 07:50:12 +0800

    enable integration graph controller,update to spring-boot 4.1.0-RC1

--- src/main/java/com/jiandong/legendaryintegration/LegendaryIntegrationApplication.java
+++ src/main/java/com/jiandong/legendaryintegration/LegendaryIntegrationApplication.java
@@ -2,11 +2,13 @@ package com.jiandong.legendaryintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.integration.http.config.EnableIntegrationGraphController;
 
+@EnableIntegrationGraphController
 @SpringBootApplication
 public class LegendaryIntegrationApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(LegendaryIntegrationApplication.class, args);
 	}
 
