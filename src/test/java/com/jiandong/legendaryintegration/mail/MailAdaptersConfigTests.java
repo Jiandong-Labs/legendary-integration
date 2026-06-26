5d15cb7ebc6289118b16c3d08744daa17bc3f11c
Jiandong Ma <jiandong.ma.cn@gmail.com>
2026-06-26 15:51:02 +0800

    mail sending & receiving integration tests
    using green mail as a mail server.
    
    use SMTP for sending and IMAP for receiving. without ssl due to cert trust issue in higher JDK.
    
    Signed-off-by: Jiandong Ma <jiandong.ma.cn@gmail.com>

---
+++ src/test/java/com/jiandong/legendaryintegration/mail/MailAdaptersConfigTests.java
@@ -0,0 +1,128 @@
+package com.jiandong.legendaryintegration.mail;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.ServerSetupTest;
+import com.jiandong.legendaryintegration.mail.parser.MimeMessageParser;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.mail.autoconfigure.MailSenderAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.endpoint.SourcePollingChannelAdapter;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.integration.test.util.TestUtils;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@EnableIntegration
+@SpringBootTest(classes = {MailAdaptersConfig.class})
+@ImportAutoConfiguration({MailSenderAutoConfiguration.class})
+@DirtiesContext
+class MailAdaptersConfigTests {
+
+	GreenMail mailServer;
+
+	@Value("${spring.mail.username}")
+	private String senderName;
+
+	@Value("${spring.mail.password}")
+	private String senderPassword;
+
+	@Value("${mail.username}")
+	private String receiverName;
+
+	@Value("${mail.password}")
+	private String receiverPassword;
+
+	@BeforeEach
+	void setup() {
+		mailServer = new GreenMail(ServerSetupTest.SMTP_IMAP);
+		mailServer.setUser(senderName, senderPassword);
+		mailServer.setUser(receiverName, receiverPassword);
+		mailServer.start();
+	}
+
+	@AfterEach
+	void tearDown() {
+		mailServer.stop();
+	}
+
+	@Autowired
+	@Qualifier("mailSendFlow.input")
+	MessageChannel mailSendingChannel;
+
+	@Autowired
+	@Qualifier("mailSendFlow.mail:outbound-channel-adapter#0")
+	private MessageHandler mailSendingHandler;
+
+	@Autowired
+	@Qualifier("imapMailPollingAdapter")
+	SourcePollingChannelAdapter imapMailPollingAdapter;
+
+	@Autowired
+	QueueChannel mailOutputChannel;
+
+	@Test
+	void happyFlow() throws MessagingException, IOException {
+		assertThat(TestUtils.<String>getPropertyValue(this.mailSendingHandler, "mailSender.host")).isEqualTo("localhost");
+		Properties javaMailProperties = TestUtils.getPropertyValue(this.mailSendingHandler, "mailSender.javaMailProperties");
+		assertThat(javaMailProperties.getProperty("mail.debug")).isEqualTo("false");
+
+		// sending to receiver
+		mailSendingChannel.send(MessageBuilder
+				.withPayload("this is a test email message.")
+				.setHeader("from", senderName)
+				.setHeader("to", new String[] {receiverName})
+				.setHeader("subject", "hello xyz")
+				.build());
+
+		// check in mail server
+		mailServer.waitForIncomingEmail(10000, 1);
+		MimeMessage[] mimeMessages = mailServer.getReceivedMessagesForDomain("cn");
+		assertThat(mimeMessages.length > 0).isTrue();
+		MimeMessage message = mimeMessages[0];
+		assertThat(message.getFrom()).containsOnly(new InternetAddress(senderName));
+		assertThat(message.getRecipients(MimeMessage.RecipientType.TO)).containsOnly(new InternetAddress(receiverName));
+		assertThat(message.getSubject()).isEqualTo("hello xyz");
+		assertThat(message.getContent()).asString().isEqualTo("this is a test email message.");
+
+		// receiving
+		imapMailPollingAdapter.start();
+
+		Message<?> mailMessage = mailOutputChannel.receive(10000);
+		Assertions.assertThat(mailMessage)
+				.extracting(Message::getPayload)
+				.isInstanceOfSatisfying(MimeMessageParser.class, mimeMessageParser -> {
+					try {
+						assertThat(mimeMessageParser.getFrom()).isEqualTo(senderName);
+						assertThat(mimeMessageParser.getTo()).containsOnly(new InternetAddress(receiverName));
+						assertThat(mimeMessageParser.getSubject()).isEqualTo("hello xyz");
+						assertThat(mimeMessageParser.getPlainContent()).isEqualTo("this is a test email message.");
+					}
+					catch (MessagingException e) {
+						throw new RuntimeException(e);
+					}
+				});
+
+		imapMailPollingAdapter.stop();
+	}
+
+}
